a9d636a63293345f876cd82dcb78f4b55ad6e6c6
Devi Hall <devihall@Devis-MacBook-Pro.local>
2022-06-27 18:32:44 -0400

    user input lowecase functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1. Prompt the user for password criteria
- //a. password length 8-128
- //b. uppercase, lowercase, numeric, special characters
-    
+//a. password length 8-128
+//b. uppercase, lowercase, numeric, special characters
+
 //2.Validate input -between 
 
 //3. generate the password
@@ -10,44 +10,56 @@
 
 ///////////////////////////////
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
-var upperCaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialChar = "!@#$%^&*()_+-="
+var numericChar = "1234567890"
+
+function generatePassword() {
+
+  var passwordLength = lengthCheck ()
+//console.log (passwordLength)
+
+  function lengthCheck() {
+    var lengthChar = window.prompt("How many characters would you like to include in your password? Choose a number between 8 - 128."); 
+    let parsedChar = parseInt(lengthChar)
+    if (parsedChar < 8) {
+      window.alert("Choose a number between 8-128")
+      return lengthCheck()
+    }
+    else if (parsedChar > 128) {
+      window.alert("Choose a number between 8-128")
+      return lengthCheck()
+    }
+    else {
+     return parsedChar
+    }
+  
+    console.log ("hello")
+  }
+  var lowercaseChar;
+  if (confirm("Would you like to include lowercase charcters? if YES, click OK")){
+
+  } 
 
 
-function generatePassword (){
-
-lengthCheck ()
-
-function lengthCheck (){
-var lengthChar = window.prompt ("How many characters would you like to include in your password? Choose a number between 8 - 128.");let parsedChar = parseInt (lengthChar)
-if (lengthChar<8) {window.alert ("Choose a number between 8-128") 
-lengthCheck ()
-}
-else if (lengthChar >128){window.alert ("Choose a number between 8-128") 
-lengthCheck ()
-}
-else {window.confirm ("Would you like to include lowercase charcters? if YES, click OK")
-}
-}
-var lowercaseChar;
-if (confirm("Would you like to include lowercase charcters? if YES, click OK")==true) { window.confirm ("Would you like to include uppercase charcters? if YES, click OK")
-
-}
+  //{ window.confirm ("Would you like to include uppercase charcters? if YES, click OK")
 
 
 
 
 
-////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////
 
 
 
-//var upperCaseChar = window.alert ("Would you like to include uppercase charcters? if YES, click OK")="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  //var upperCaseChar = window.alert ("Would you like to include uppercase charcters? if YES, click OK")="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-//var specialChar = window.alert ("Would you like to include special charcters? if YES, click OK")="!@#$%^&*()_+-="
+  //var specialChar = window.alert ("Would you like to include special charcters? if YES, click OK")="!@#$%^&*()_+-="
 
-//var numericChar = window.alert ("Would you like to include numeric charcters? if YES, click OK")="1234567890"
-
+  //var numericChar = window.alert ("Would you like to include numeric charcters? if YES, click OK")="1234567890"
+return "Example"
 }
 
 // Assignment Code
