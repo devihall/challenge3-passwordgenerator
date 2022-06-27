340b96b0c8471a586bcbd18ac4b196e8ef277327
Devi Hall <devihall@Devis-MacBook-Pro.local>
2022-06-27 11:56:24 -0400

    password character length

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 //1. Prompt the user for password criteria
-     //a. password length 8-128
+    function generatePassword () {
+    
+      //a. password length 8-128
+    
+      var passwordLength= window.prompt ("Choose a password length between 8-128")
+      if (passwordLength <8) {
+        window.alert ("Choose a number between 8-128")}
+        else (passwordLength >8) { 
+          window.alert ("Do you want numeric characters in your password? ")
+      }
+    }
+    
+
      //b. uppercase, lowercase, numeric, special characters
+    
+
+
 
 //2.Validate input -between 8-128 and atleast one char type is being selected
 // maybe this is while loop?
