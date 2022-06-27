5b077bfdef9ef7232f02f97fb366a1a25240648e
Devi Hall <devihall@Devis-MacBook-Pro.local>
2022-06-27 17:00:06 -0400

    password lowercase and uppercase input

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 //1. Prompt the user for password criteria
-    function generatePassword () {
+ //a. password length 8-128
+ //b. uppercase, lowercase, numeric, special characters
     
-      //a. password length 8-128
-    
-      var passwordLength= window.prompt ("Choose a password length between 8-128")
-      if (passwordLength <8) {
-        window.alert ("Choose a number between 8-128")}
-        else (passwordLength >8) { 
-          window.alert ("Do you want numeric characters in your password? ")
-      }
-    }
-    
-
-     //b. uppercase, lowercase, numeric, special characters
-    
-
-
-
-//2.Validate input -between 8-128 and atleast one char type is being selected
-// maybe this is while loop?
-//while the number is between 1-128 do this
+//2.Validate input -between 
 
 //3. generate the password
 
 //4. display the password
 
+///////////////////////////////
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
+var upperCaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
+function generatePassword (){
+
+lengthCheck ()
+
+function lengthCheck (){
+var lengthChar = window.prompt ("How many characters would you like to include in your password? Choose a number between 8 - 128.");let parsedChar = parseInt (lengthChar)
+if (lengthChar<8) {window.alert ("Choose a number between 8-128") 
+lengthCheck ()
+}
+else if (lengthChar >128){window.alert ("Choose a number between 8-128") 
+lengthCheck ()
+}
+else {window.confirm ("Would you like to include lowercase charcters? if YES, click OK")
+}
+}
+var lowercaseChar;
+if (confirm("Would you like to include lowercase charcters? if YES, click OK")==true) { window.confirm ("Would you like to include uppercase charcters? if YES, click OK")
+
+}
+
+
+
+
+
+////////////////////////////////////////////////////
+
+
+
+//var upperCaseChar = window.alert ("Would you like to include uppercase charcters? if YES, click OK")="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+//var specialChar = window.alert ("Would you like to include special charcters? if YES, click OK")="!@#$%^&*()_+-="
+
+//var numericChar = window.alert ("Would you like to include numeric charcters? if YES, click OK")="1234567890"
+
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
